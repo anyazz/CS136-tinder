d915a6f4614ec8dec22da3445880316bf1c4276f
anyazz <anyazhang01@yahoo.com>
2018-12-09 19:19:05 -0500

    started tinder.py

--- tinder.py
+++ tinder.py
@@ -4,3 +4,32 @@
 # 1. attempt to handle matches sequentially like Tinder
 # 2. run algorithm once w/ random subsets to get predictions, 
 #	 use predictions to choose new subsets, calculate utilities
+
+
+from user import User
+import random
+users = []
+
+def init_users(n):
+	global users
+	for i in range(n):
+		users.append(User(i))
+
+def run_round(isRandom):
+	sorted_users = users
+	candidates = {}
+	if not isRandom:
+		sorted_users.sort(key=lambda x: x.p_hat, reverse=True)		
+	for user in sorted_users:	
+		if user.id not in candidates:
+			user_ids = set(i for i in range(len(users)))
+			if not isRandom: 
+				pass
+			x = random.sample(user_ids - user.seen - set([user.id]), 1)[0]
+			candidates[user.id] = users[x].id
+			candidates[users[x].id] = user.id
+	print candidates
+
+init_users(6)
+for i in range(5):
+	run_round(True)
--- user.py
+++ user.py
@@ -9,4 +9,10 @@
 # swipe - takes other User object, returns 1 for swipe right and 0 for swipe left
 # 		  (possibly nondeterministic --> p of not defecting = abs(rating - threshold)/5)	
 # get_utility - calculates utility to user
-# update_predictions - based on outcome of swipe, update r_hat and p_hat
+# update_predictions - based on outcome of swipe, update other user's r_hat and current user's p_hat
+
+
+class User:
+	def __init__(self, id):
+		self.id = id
+		self.seen = set()
