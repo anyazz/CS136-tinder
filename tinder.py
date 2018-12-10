608732e0501414cdbc61046cca3132fa485030b9
anyazz <anyazhang01@yahoo.com>
2018-12-10 00:43:27 -0500

    cleaned code, removed requirement that each  random round has distinct candidates

--- tinder.py
+++ tinder.py
@@ -9,56 +9,68 @@ from user import User
 import numpy as np
 import random
 users = []
+discount = 1.0
 
-def init_users(n):
-    global users
-    for i in range(n):
-        users.append(User(i))
-
-def run_round(i, isRandom):
-    flag = False
-    sorted_users = users
+def run_round(i):
     candidates = {}
-    if not isRandom:
-        sorted_users.sort(key=lambda x: x.p_hat, reverse=True)
-    for user in sorted_users:
-        if user.id not in candidates:
-            user_ids = set(i for i in range(len(users)))
-            if not isRandom:
-                pass
-            sample = user_ids - user.seen - set([user.id]) - set(candidates.keys())
-            x = random.sample(sample, 1)[0]
-            candidates[user.id] = x
-            candidates[x] = user.id
-            user.seen.add(x)
-            users[x].seen.add(user.id)
+    user_ids = set(i for i in range(len(users)))
+    for user in users:
+        sample = user_ids - set([user.id]) - set(user.seen)
+        x = random.choice(tuple(sample))
+        candidates[user.id] = x
+        user.seen.add(x)
+
+    # print(candidates)
     for user_id, cand_id in candidates.items():
         user = users[user_id]
         cand = users[cand_id]
-        user.swipe(cand)
-        user.history.append((i, cand_id, user.r_hat, user.p_hat))
-        # print(users[user_id])
+        diff = user.swipe(cand, discount, isTraining=True)
+        user.history.append((i, cand_id, round(user.r_hat,2), round(user.p_hat,2), diff))
 
 def rmse(predictions, targets):
     return np.sqrt(((predictions - targets) ** 2).mean())
 
-init_users(500)
-for i in range(200):
-    while True:
-        try:
-            run_round(i, (True))
-            break
-        except:
-            pass
+def tinder(n, rounds):
+    global users, discount
+    discount = .1 ** (1/(2*rounds))
 
-real_r, pred_r, real_p, pred_p = [], [], [], []
-for user in users:
-    real_r.append(user.r)
-    real_p.append(user.p)
-    pred_r.append(user.r_hat)
-    pred_p.append(user.p_hat)
-    print(user)
-    # print("History: " + str(user.history))
-print("Delta: " + str(users[0].delta))
-print(rmse(np.array(real_r), np.array(pred_r)))
-print(rmse(np.array(real_p), np.array(pred_p)))
+    # initialize users
+    users = []
+    for i in range(n):
+        user = User(i)
+        users.append(User(i))
+
+    # train predictions using random pairs of candidates
+    for i in range(rounds):
+        run_round(i)
+
+    # calculate RMSE
+    real_r, pred_r, real_p, pred_p = [], [], [], []
+    for user in users:
+        real_r.append(user.r)
+        real_p.append(user.p)
+        pred_r.append(user.r_hat)
+        pred_p.append(user.p_hat)
+        print(user)
+        # print("History: " + str(user.history))
+        # print("Delta: " + str(user.delta))
+    return (rmse(np.array(real_r), np.array(pred_r)), rmse(np.array(real_p), np.array(pred_p)))
+
+    # simulate actual Tinder, calculating total utility
+    # utility = 0
+    # for user in users:
+    #     candidates = [x for x in users if x.r >= user.p and user.r >= x.p]
+    #     for candidate in candidates:
+    #         user.swipe(candidate, isTraining=false)
+    #     utility += user.utility
+    # print("TOTAL U", utility)
+
+sum_rmse_r, sum_rmse_p = 0.0,0.0
+for i in range(10):
+    print("****TEST {}****".format(i))
+    rmse_r, rmse_p = tinder(300, 200)
+    sum_rmse_r += rmse_r
+    sum_rmse_p += rmse_p
+print(sum_rmse_r/10, sum_rmse_p/10)
+
+
--- user.py
+++ user.py
@@ -1,11 +1,10 @@
-# User class
+# other class
 
 # ATTRIBUTES
 # user id
 # true values for rating and pickiness, all i.i.d Unif(0, 5)
 # predicted values for rating and pickiness, init to 2.5
 import random
-discount = 0.99
 
 class User:
 
@@ -19,6 +18,8 @@ class User:
         self.matches = set()
         self.delta = 1.0
         self.history = []
+        self.utility = 0
+        self.discount = 1.0
 
     def __str__(self):
         string = "User {}: \n    true: {}, {}\n    pred: {}, {}"
@@ -27,20 +28,43 @@ class User:
 # METHODS
 # swipe - takes other User object, returns 1 for swipe right and 0 for swipe left
 #         (possibly nondeterministic --> p of not defecting = abs(rating - threshold)/5)
-# get_utility - calculates utility to user
+# get_utility - calculates utility to other
 # update_predictions - based on outcome of swipe, update r_hat and p_hat
-    def swipe(self, user):
-        diff = abs(self.p_hat - user.r_hat) 
-        diff = max(diff, 1) * self.delta
-        if self.p <= user.r:
-            # print("User {} swiped right on user {}".format(self.id, user.id))
-            if self.p_hat > user.r_hat:
-                self.p_hat = round(max(self.p_hat - diff, 0.0), 1)
-                user.r_hat = round(min(user.r_hat + diff, 5.0), 1)
+    def swipe(self, other, discount, isTraining):
+        if isTraining:
+            diff = self.train(other, discount)
         else:
-            # print("User {} swiped left on user {}".format(self.id, user.id))
-            if self.p_hat <= user.r_hat:
-                self.p_hat = round(min(self.p_hat + diff, 5.0), 1)
-                user.r_hat = round(max(user.r_hat - diff, 0.0), 1)
+            self.get_utility(other)
+        return diff
+
+    def train(self, other, discount):
+        diff = abs(self.p_hat - other.r_hat) 
+        self_diff = max(diff, 1) * self.delta
+        other_diff = max(diff, 1) * other.delta
+
+        if self.p <= other.r and self.p_hat > other.r_hat:
+            self.p_hat = round(max(self.p_hat - self_diff, 0.0), 2)
+            other.r_hat = round(min(other.r_hat + other_diff, 5.0), 2)
+        elif self.p > other.r and self.p_hat <= other.r_hat:
+            self.p_hat = round(min(self.p_hat + self_diff, 5.0), 2)
+            other.r_hat = round(max(other.r_hat - other_diff, 0.0), 2)
         self.delta = self.delta * discount
+        other.delta = other.delta * discount
+        return self_diff
+
+    def get_utility(self, other):
+        if self.p <= other.r:
+            # swipe right
+            if other.p <= self.r:
+                # match
+                self.utility += max(0, other.r - self.r) + 1
+            else:
+                self.utility += min(0, other.r - self.r) - 1
+        else:
+            # swipe left
+            if other.p <= self.r:
+                self.utility += 0.5
+            else:
+                self.utility += 0
         return
+
