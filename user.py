8de81d13ed4dde0bb1b72c4ad503e910e0e185f6
anyazz <anyazhang01@yahoo.com>
2018-12-09 19:44:14 -0500

    added uniformly selected r, p

--- user.py
+++ user.py
@@ -4,12 +4,14 @@
 # user id
 # true values for rating and pickiness, all i.i.d Unif(0, 5)
 # predicted values for rating and pickiness, init to 2.5
+import random
+
 class User:
 
-    def __init__(self, id, r, p):
+    def __init__(self, id):
         self.id = id
-        self.r = r
-        self.p = p
+        self.r = round(random.uniform(0.0, 5.0), 1)
+        self.p = round(random.uniform(0.0, 5.0), 1)
         self.r_hat = 2.5
         self.p_hat = 2.5
         self.seen = set()
@@ -23,19 +25,13 @@ class User:
 # update_predictions - based on outcome of swipe, update r_hat and p_hat
     def swipe(self, user):
 		diff = (self.p_hat - user.r_hat) * discount
-        if self.p <= user.r:
-        	if self.p_hat > user.r_hat:
-            	self.p_hat -= diff
-            	user.r_hat += diff
-        else:
-            if self.p_hat <= user.r_hat:
-            	self.p_hat += diff
-            	user.r_hat -= diff
-        return
+		if self.p <= user.r:
+			if self.p_hat > user.r_hat:
+				self.p_hat -= diff
+				user.r_hat += diff
+		else:
+			if self.p_hat <= user.r_hat:
+				self.p_hat -= diff
+				user.r_hat += diff
+		return
 
-    def swiped(self, direction, user):
-        if direction:
-            self.rating += userPick * discount
-        else:
-            self.rating -= userPick * discount
-        return
