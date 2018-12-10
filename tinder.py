1f9762b3790093ed01ac604f49dc25f768db208f
Galen Lee <galenlee411@gmail.com>
2018-12-09 21:20:09 -0500

    fixed sample errors

--- tinder.py
+++ tinder.py
@@ -12,26 +12,32 @@ users = []
 def init_users(n):
 	global users
 	for i in range(n):
-		users.append(User(i), 5, 5)
+		users.append(User(i))
 
 def run_round(isRandom):
-	sorted_users = users
-	candidates = {}
-	if not isRandom:
-		sorted_users.sort(key=lambda x: x.p_hat, reverse=True)
-	for user in sorted_users:
-		if user.id not in candidates:
-			user_ids = set(i for i in range(len(users)))
-			if not isRandom:
-				pass
-			sample = user_ids - user.seen - set([user.id]) - set(candidates.keys())
-			x = random.sample(sample, 1)[0]
-			candidates[user.id] = x
-			candidates[x] = user.id
-			user.seen.add(x)
-			users[x].seen.add(user.id)
-	print candidates
+    flag = False
+    sorted_users = users
+    candidates = {}
+    if not isRandom:
+        sorted_users.sort(key=lambda x: x.p_hat, reverse=True)
+    for user in sorted_users:
+        if user.id not in candidates:
+            user_ids = set(i for i in range(len(users)))
+            if not isRandom:
+                pass
+            sample = user_ids - user.seen - set([user.id]) - set(candidates.keys())
+            x = random.sample(sample, 1)[0]
+            candidates[user.id] = x
+            candidates[x] = user.id
+            user.seen.add(x)
+            users[x].seen.add(user.id)
+    print candidates
 
-init_users(6)
+init_users(10)
 for i in range(5):
-	run_round(True)
+    while True:
+        try:
+    	    run_round(True)
+            break
+        except:
+            pass
