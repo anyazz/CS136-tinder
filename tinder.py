cd6068220254f52310fd1ee03b85abc1a8af1d91
anyazz <anyazhang01@yahoo.com>
2018-12-09 21:22:25 -0500

    added user history, fixed diff

--- tinder.py
+++ tinder.py
@@ -3,35 +3,48 @@
 # options:
 # 1. attempt to handle matches sequentially like Tinder
 # 2. run algorithm once w/ random subsets to get predictions,
-#	 use predictions to choose new subsets, calculate utilities
+#    use predictions to choose new subsets, calculate utilities
 
 from user import User
 import random
 users = []
 
 def init_users(n):
-	global users
-	for i in range(n):
-		users.append(User(i), 5, 5)
+    global users
+    for i in range(n):
+        users.append(User(i))
 
-def run_round(isRandom):
-	sorted_users = users
-	candidates = {}
-	if not isRandom:
-		sorted_users.sort(key=lambda x: x.p_hat, reverse=True)
-	for user in sorted_users:
-		if user.id not in candidates:
-			user_ids = set(i for i in range(len(users)))
-			if not isRandom:
-				pass
-			sample = user_ids - user.seen - set([user.id]) - set(candidates.keys())
-			x = random.sample(sample, 1)[0]
-			candidates[user.id] = x
-			candidates[x] = user.id
-			user.seen.add(x)
-			users[x].seen.add(user.id)
-	print candidates
+def run_round(i, isRandom):
+    sorted_users = users
+    candidates = {}
+    if not isRandom:
+        sorted_users.sort(key=lambda x: x.p_hat, reverse=True)
+    for user in sorted_users:
+        if user.id not in candidates:
+            user_ids = set(i for i in range(len(users)))
+            if not isRandom:
+                pass
+            sample = user_ids - user.seen - set([user.id]) - set(candidates.keys())
+            x = random.sample(sample, 1)[0]
+            candidates[user.id] = x
+            candidates[x] = user.id
+            user.seen.add(x)
+            users[x].seen.add(user.id)
+    print(candidates)
+    for user_id, cand_id in candidates.items():
+        user = users[user_id]
+        cand = users[cand_id]
+        user.swipe(cand)
+        user.history.append((i, cand_id, user.r_hat, user.p_hat))
+        # print(users[user_id])
 
-init_users(6)
-for i in range(5):
-	run_round(True)
+
+init_users(100)
+for i in range(100):
+    print("********ROUND {}****************".format(i))
+    try:
+        run_round(i, True)
+    except:
+        for user in users:
+            print(user, "\n", user.history)
+        break
--- user.py
+++ user.py
@@ -16,21 +16,29 @@ class User:
         self.p_hat = 2.5
         self.seen = set()
         self.matches = set()
-        self.discount = 0.01
+        self.discount = 0.3
+        self.history = []
+
+    def __str__(self):
+        string = "User {}: \n    true: {}, {}\n    pred: {}, {}"
+        return string.format(self.id, self.r, self.p, self.r_hat, self.p_hat)
 
 # METHODS
 # swipe - takes other User object, returns 1 for swipe right and 0 for swipe left
-# 		  (possibly nondeterministic --> p of not defecting = abs(rating - threshold)/5)
+#         (possibly nondeterministic --> p of not defecting = abs(rating - threshold)/5)
 # get_utility - calculates utility to user
 # update_predictions - based on outcome of swipe, update r_hat and p_hat
     def swipe(self, user):
-        diff = (self.p_hat - user.r_hat) * discount
+        diff = abs(self.p_hat - user.r_hat) * self.discount
+        diff = max(diff, self.discount)
         if self.p <= user.r:
+            # print("User {} swiped right on user {}".format(self.id, user.id))
             if self.p_hat > user.r_hat:
-            	self.p_hat = max(self.p_hat - diff, 0)
-            	user.r_hat = min(user.r_hat + diff, 5)
+                self.p_hat = round(max(self.p_hat - diff, 0.0), 1)
+                user.r_hat = round(min(user.r_hat + diff, 5.0), 1)
         else:
+            # print("User {} swiped left on user {}".format(self.id, user.id))
             if self.p_hat <= user.r_hat:
-            	self.p_hat = min(self.p_hat - diff, 5)
-            	user.r_hat = max(user.r_hat + diff, 0)
+                self.p_hat = round(min(self.p_hat + diff, 5.0), 1)
+                user.r_hat = round(max(user.r_hat - diff, 0.0), 1)
         return
